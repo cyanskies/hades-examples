8e425a50342e00e9bb5348b83b74898317d4dada
Steven Pilkington <cyanskies@users.noreply.github.com>
2018-08-08 12:07:08 +1000

    added framework for bounce example

---
+++ bounce/source/main.cpp
@@ -0,0 +1,35 @@
+#include "Hades/App.hpp"
+#include "Hades/Main.hpp"
+
+#include "Objects/editor.hpp"
+
+#include "bounce_state.hpp"
+#include "bounce_systems.hpp"
+
+int main(int argc, char **argv)
+{
+	return hades_main(argc, argv);
+}
+
+std::string_view defaultGame()
+{
+	return "bounce";
+}
+
+void resourceTypes(hades::data::data_system &data)
+{
+	//add bounce systems
+}
+
+void hadesMain(hades::StateManager &state, hades::input_system &bindings, hades::command_list &commandLine)
+{
+	const auto editor = hades::LoadCommand(commandLine, "editor", [&state](auto &&args) {
+		state.push(std::make_unique<objects::object_editor>());
+		return true;
+	});
+
+	if (editor)
+		state.push(std::make_unique<objects::object_editor>());
+	else
+		;//push bounce state
+}
